cf8f9c875be88db3c37fe5ee18b7eb011fc2a8c7
kmwhelan93 <kmw8sf@virginia.edu>
2015-01-21 18:23:50 -0500

    modified authentication method

--- src/main/java/com/example/AuthenticateResource.java
+++ src/main/java/com/example/AuthenticateResource.java
@@ -1,9 +1,12 @@
 package com.example;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.Consumes;
 
 
 import javax.ws.rs.core.MediaType;
@@ -15,11 +18,12 @@ import jsonObjects.AuthenticateBean;
 public class AuthenticateResource {
 	public static HelloWorld hw = new HelloWorld();
 	
-	@GET
+	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public AuthenticateBean authenticate(@QueryParam("username") String username,
-			@QueryParam("password") String password) {
+	public AuthenticateBean authenticate(@FormParam("username") String username,
+			@FormParam("password") String password) {
 		boolean result = hw.authenticate(username, password);
+		System.out.println("Authentication request: " + username + " " + password);
 		return new AuthenticateBean(result);
 	}
 }
